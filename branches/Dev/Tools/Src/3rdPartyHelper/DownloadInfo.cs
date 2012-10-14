e859674124c980203d86957e3044cdc257b9a68e
victor.sushko@gmail.com <victor.sushko@gmail.com@01a7efd1-0ae0-fa29-c58a-2c4763e9e309>
2012-10-14 12:26:25 +0000

    Script & tool for downloading 3rdParty libraries.

---
+++ branches/Dev/Tools/Src/3rdPartyHelper/DownloadInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ThirdPartyHelper
+{
+    class DownloadInfo
+    {
+        private static readonly Regex StringPattern = new Regex(@"""[^""]*""|[^""\s]*");
+
+        public string Uri { get; set; }
+        public string HashCode { get; set; }
+        public string SourcePath { get; set; }
+        public string DestinationPath { get; set; }
+
+        public static DownloadInfo Parse(string str)
+        {
+            var matches = StringPattern.Matches(str).Cast<Match>().Where(m => m.Length > 0).ToArray();
+            if (matches.Length != 4)
+                throw new InvalidOperationException(string.Format("String '{0}' does not match the following pattern: '\"Uri\" \"HashCode\" \"SourcePath\" \"DestinationPath\"", str));
+
+            var result = new DownloadInfo();
+            for(int i=0; i<4; i++)
+            {
+                var value = matches[i].Value.Trim(' ', '"');
+                switch (i)
+                {
+                    case 0:
+                        result.Uri = value;
+                        break;
+                    case 1:
+                        result.HashCode = value;
+                        break;
+                    case 2:
+                        result.SourcePath = value;
+                        break;
+                    case 3:
+                        result.DestinationPath = value;
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
