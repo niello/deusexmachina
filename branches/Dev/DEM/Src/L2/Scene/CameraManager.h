ada628286df54dfe5519d40f48dfa15ba6a7867b
niello11@gmail.com <niello11@gmail.com@01a7efd1-0ae0-fa29-c58a-2c4763e9e309>
2013-06-12 22:20:16 +0000

    Camera manager made per-level, camera state save-load

--- branches/Dev/DEM/Src/L2/Scene/CameraManager.h
+++ branches/Dev/DEM/Src/L2/Scene/CameraManager.h
@@ -8,12 +8,10 @@
 //#include <Scene/Camera.h>
 //#include <util/npfeedbackloop.h>
 
-// Manages the main camera of the active level. Provides interface for driving camera
+// Manages the current camera of the game level. Provides interface for driving camera
 // (manually, by input, by script), performing smooth transition between changing targets,
 // chasing or tracking some scene node, shaking to simulate explosions and quakes etc.
 // Camera manager also takes care of camera collisions and target size dependent limiting.
-
-//???autoadjust camera aspect here or in scene?
 
 //!!!L2 CameraManager:
 // Third-person COI rotation with 2-sided vertical angle limitimg
@@ -24,43 +22,26 @@
 // Set camera (which camera to use)
 // Focus on entity - smooth move from curr state to new object
 
-//BEGIN_ATTRS_REGISTRATION(PropChaseCamera)
-//	RegisterFloatWithDefault(CameraDistance, ReadOnly, 7.0f);
-//	RegisterFloatWithDefault(CameraMinDistance, ReadOnly, 1.5f);
-//	RegisterFloatWithDefault(CameraMaxDistance, ReadOnly, 15.0f);
-//	RegisterFloatWithDefault(CameraAngularVelocity, ReadOnly, 6.0f);
-//	RegisterVector3WithDefault(CameraOffset, ReadOnly, vector4(0.0f, 1.5f, 0.0f, 0.f));
-//	RegisterFloatWithDefault(CameraLowStop, ReadOnly, 5.0f);
-//	RegisterFloatWithDefault(CameraHighStop, ReadOnly, 45.0f);
-//	RegisterFloatWithDefault(CameraDistanceStep, ReadOnly, 1.0f);
-//	RegisterFloatWithDefault(CameraLinearGain, ReadOnly, -10.0f);
-//	RegisterFloatWithDefault(CameraAngularGain, ReadOnly, -15.0f);
-//	RegisterFloatWithDefault(CameraDefaultTheta, ReadOnly, 20.0f);
-//	//???DefineFloatWithDefault(CameraDefaultRho, 'CRHO', ReadWrite, n_deg2rad(10.0f));?
-//END_ATTRS_REGISTRATION
-
 namespace Scene
 {
-	class CSceneNode;
-}
-
-namespace Game
-{
-#define CameraMgr Game::CCameraManager::Instance()
+class CScene;
+class CSceneNode;
 
 class CCameraManager: public Core::CRefCounted
 {
 	__DeclareClassNoFactory;
-	__DeclareSingleton(CCameraManager);
 
 protected:
 
-	bool					IsThirdPerson; // Cached to avoid RTTI checking. //???remove? Strict IsA() is fast
-	Scene::CSceneNode*		pCameraNode;
-	Scene::PNodeController	Ctlr;
+	bool			IsThirdPerson; //???need? // Cached to avoid RTTI checking
+	CScene*			pScene;
+	CSceneNode*		pCameraNode;
+	PNodeController	Ctlr;
 
 	//nPFeedbackLoop<vector3>				Position;
 	//nPFeedbackLoop<vector3>				Lookat;
+
+	//accelerations, if smoothing needed
 
 	//collision
 	//transition //???+ spline animation? - using node for COI of 3person camera is good for animated COI!
@@ -90,23 +71,23 @@ public:
 	float	Sensitivity;
 	float	MoveSpeed;
 	float	ZoomSpeed;
+	//???RotationSpeed; , or it is a sensitivity?
 	//???bool InvertY?
 
-	CCameraManager(): pCameraNode(NULL), Sensitivity(0.05f), MoveSpeed(0.1f), ZoomSpeed(1.f) { __ConstructSingleton; }
-	~CCameraManager() { __DestructSingleton; }
+	CCameraManager(): pCameraNode(NULL), Sensitivity(0.05f), MoveSpeed(0.1f), ZoomSpeed(1.f) { }
 
-	bool					InitThirdPersonCamera(); //create controller (to attach to cameras later), attach if has active lvel //???setup aspect?
-	void					ResetCamera(); //???reset only orientation (and distance for 3P)?
+	bool				InitThirdPersonCamera(CScene& Scene, CSceneNode* pNodeWithCamera = NULL);
+	void				ResetCamera(); //???reset only orientation (and distance for 3P)?
 
-	void					SetTarget(const Scene::CSceneNode* pTarget); //!!!assert target is in the same scene as the camera!
-	void					SetTargetTrackingMode(ETrackingMode Mode);
+	void				SetTarget(const CSceneNode* pTarget, const vector3& Offset = vector3::Zero); //!!!assert target is in the same scene as the camera!
+	void				SetTargetTrackingMode(ETrackingMode Mode);
 
-	void					EnableCollision(bool Enable);
+	void				EnableCollision(bool Enable);
 	//void PreserveDistance(); // When object/terrain is under a camera, follow its surface
 
-	Scene::CSceneNode*		GetCameraNode() const { return pCameraNode; }
-	Scene::CNodeController*	GetCameraController() const { return Ctlr.GetUnsafe(); }
-	bool					IsCameraThirdPerson() const { return IsThirdPerson; }
+	CSceneNode*			GetCameraNode() const { return pCameraNode; }
+	CNodeController*	GetCameraController() const { return Ctlr.GetUnsafe(); }
+	bool				IsCameraThirdPerson() const { return IsThirdPerson; }
 
 	//???force COI in parent space for 3P camera? default is zero vector
 	//chasing is attaching camera to some entity
