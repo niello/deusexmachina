547a54a9664a9a20848e4cba7b723ab5d4ec248b
niello11@gmail.com <niello11@gmail.com@01a7efd1-0ae0-fa29-c58a-2c4763e9e309>
2013-06-26 06:10:01 +0000

    BB progress, NPK TOC formed

---
+++ branches/Dev/Tools/Src/BBuilder/Descs.cpp
@@ -0,0 +1,479 @@
+#include "Main.h"
+
+#include <IO/IOServer.h>
+#include <IO/Streams/FileStream.h>
+#include <IO/BinaryWriter.h>
+#include <Data/DataServer.h>
+#include <Data/DataArray.h>
+
+bool ProcessDialogue(const nString& SrcContext, const nString& ExportContext, const nString& Name)
+{
+	nString ExportFilePath = ExportContext + Name + ".prm";
+
+	if (IsFileAdded(ExportFilePath)) OK;
+
+	Data::PParams Desc;
+	if (ExportDescs)
+	{
+		IOSrv->CreateDirectory(ExportFilePath.ExtractDirName());
+		Desc = DataSrv->LoadHRD(SrcContext + Name + ".dlg", false);
+		if (!DataSrv->SavePRM(ExportFilePath, Desc)) FAIL;
+	}
+	else Desc = DataSrv->LoadPRM(ExportFilePath);
+
+	if (!Desc.IsValid()) FAIL;
+
+	FilesToPack.InsertSorted(ExportFilePath);
+
+	bool UsesScript = false;
+
+	const Data::CDataArray& Links = *(Desc->Get<Data::PDataArray>(CStrID("Links")));
+	for (int j = 0; j < Links.GetCount(); j++)
+	{
+		//const Data::CDataArray& Link = *(Links[j]); // Crashes vs2008 compiler :)
+		const Data::CDataArray& Link = *(Links.Get(j).GetValue<Data::PDataArray>());
+
+		if (Link.GetCount() > 2)
+		{
+			if (Link.Get(2).GetValue<nString>().IsValid()) UsesScript = true;
+			else if (Link.GetCount() > 3 && Link.Get(3).GetValue<nString>().IsValid()) UsesScript = true;
+			if (UsesScript)
+			{
+				int Idx = Desc->IndexOf(CStrID("ScriptFile"));
+				nString ScriptFile = (Idx == INVALID_INDEX) ? Name : Desc->Get(Idx).GetValue<nString>();
+
+				ExportFilePath = ExportContext + ScriptFile + ".lua";
+
+				if (IsFileAdded(ExportFilePath)) break;
+
+				if (ExportDescs)
+				{
+					CFLuaIn.Append(SrcContext + ScriptFile + ".lua");
+					CFLuaOut.Append(ExportFilePath);
+				}
+				FilesToPack.InsertSorted(ExportFilePath);
+
+				break;
+			}
+		}
+	}
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool ProcessCollisionShape(const nString& SrcFilePath, const nString& ExportFilePath)
+{
+	if (IsFileAdded(ExportFilePath)) OK;
+
+	Data::PParams Desc;
+	if (ExportDescs)
+	{
+		IOSrv->CreateDirectory(ExportFilePath.ExtractDirName());
+		Desc = DataSrv->LoadHRD(SrcFilePath, false);
+		if (!DataSrv->SavePRM(ExportFilePath, Desc)) FAIL;
+	}
+	else Desc = DataSrv->LoadPRM(ExportFilePath);
+
+	if (!Desc.IsValid()) FAIL;
+
+	FilesToPack.InsertSorted(ExportFilePath);
+
+	// Add terrain file for heightfield shapes (always exported) //???or allow building from L3DT src?
+	nString FileName = Desc->Get(CStrID("FileName"), nString::Empty);
+	if (!FileName.IsEmpty() && !IsFileAdded(FileName)) FilesToPack.InsertSorted(FileName);
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool ProcessPhysicsDesc(const nString& SrcFilePath, const nString& ExportFilePath)
+{
+	if (IsFileAdded(ExportFilePath)) OK;
+
+	Data::PParams Desc;
+	if (ExportDescs)
+	{
+		IOSrv->CreateDirectory(ExportFilePath.ExtractDirName());
+		Desc = DataSrv->LoadHRD(SrcFilePath, false);
+		if (!DataSrv->SavePRM(ExportFilePath, Desc)) FAIL;
+	}
+	else Desc = DataSrv->LoadPRM(ExportFilePath);
+
+	if (!Desc.IsValid()) FAIL;
+
+	FilesToPack.InsertSorted(ExportFilePath);
+
+	Data::PDataArray Objects;
+	if (Desc->Get(Objects, CStrID("Objects")))
+	{
+		for (int i = 0; i < Objects->GetCount(); ++i)
+		{
+			Data::PParams ObjDesc = Objects->Get<Data::PParams>(i);
+			CStrID PickShape = ObjDesc->Get<CStrID>(CStrID("Shape"), CStrID::Empty);
+			if (PickShape.IsValid())
+				if (!ProcessCollisionShape(	nString("Src:Physics/") + PickShape.CStr() + ".hrd",
+											nString("Export:Physics/") + PickShape.CStr() + ".prm"))
+				{
+					n_msg(VR_ERROR, "Error processing collision shape '%s'\n", PickShape.CStr());
+					FAIL;
+				}
+		}
+	}
+
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool ProcessAnimDesc(const nString& SrcFilePath, const nString& ExportFilePath)
+{
+	if (IsFileAdded(ExportFilePath)) OK;
+
+	Data::PParams Desc;
+	if (ExportDescs)
+	{
+		IOSrv->CreateDirectory(ExportFilePath.ExtractDirName());
+		Desc = DataSrv->LoadHRD(SrcFilePath, false);
+		if (!DataSrv->SavePRM(ExportFilePath, Desc)) FAIL;
+	}
+	else Desc = DataSrv->LoadPRM(ExportFilePath);
+
+	if (!Desc.IsValid()) FAIL;
+
+	FilesToPack.InsertSorted(ExportFilePath);
+
+	for (int i = 0; i < Desc->GetCount(); ++i)
+	{
+		//???!!!allow compile or batch-compile?
+		// can add Model resource description, associated with this anim, to CFModel list
+		nString FileName = nString("Export:Anims/") + Desc->Get(i).GetValue<CStrID>().CStr();
+		if (!IsFileAdded(FileName)) FilesToPack.InsertSorted(FileName);
+	}
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+//???pre-unwind descs on exports?
+bool ProcessDescWithParents(const nString& SrcContext, const nString& ExportContext, const nString& Name)
+{
+	nString ExportFilePath = ExportContext + Name + ".prm";
+
+	if (IsFileAdded(ExportFilePath)) OK;
+
+	Data::PParams Desc;
+	if (ExportDescs)
+	{
+		IOSrv->CreateDirectory(ExportFilePath.ExtractDirName());
+		Desc = DataSrv->LoadHRD(SrcContext + Name + ".hrd", false);
+		if (!DataSrv->SavePRM(ExportFilePath, Desc)) FAIL;
+	}
+	else Desc = DataSrv->LoadPRM(ExportFilePath);
+
+	if (!Desc.IsValid()) FAIL;
+
+	FilesToPack.InsertSorted(ExportFilePath);
+
+	nString BaseName = Desc->Get(CStrID("_Base_"), nString::Empty);
+	return BaseName.IsEmpty() || (BaseName != Name && ProcessDescWithParents(SrcContext, ExportContext, BaseName));
+}
+//---------------------------------------------------------------------
+
+bool ProcessMaterialDesc(const nString& Name)
+{
+	nString ExportFilePath = "Export:Materials/" + Name + ".prm";
+
+	if (IsFileAdded(ExportFilePath)) OK;
+
+	Data::PParams Desc;
+	if (ExportDescs)
+	{
+		IOSrv->CreateDirectory(ExportFilePath.ExtractDirName());
+		Desc = DataSrv->LoadHRD("Src:Materials/" + Name + ".hrd", false);
+		if (!DataSrv->SavePRM(ExportFilePath, Desc)) FAIL;
+	}
+	else Desc = DataSrv->LoadPRM(ExportFilePath);
+
+	if (!Desc.IsValid()) FAIL;
+
+	FilesToPack.InsertSorted(ExportFilePath);
+
+	Data::PParams Textures;
+	if (Desc->Get(Textures, CStrID("Textures")))
+	{
+		//!!!when export from src, find resource desc and add source texture to CFTexture list!
+		for (int i = 0; i < Textures->GetCount(); ++i)
+		{
+			nString FileName = nString("Export:Textures/") + Textures->Get(i).GetValue<CStrID>().CStr();
+			if (!IsFileAdded(FileName)) FilesToPack.InsertSorted(FileName);
+		}
+	}
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool ProcessSceneNodeRefs(const Data::CParams& NodeDesc)
+{
+	Data::PDataArray Attrs;
+	if (NodeDesc.Get(Attrs, CStrID("Attrs")))
+	{
+		for (int i = 0; i < Attrs->GetCount(); ++i)
+		{
+			Data::PParams AttrDesc = Attrs->Get<Data::PParams>(i);
+
+			Data::PParams Textures;
+			if (AttrDesc->Get(Textures, CStrID("Textures")))
+			{
+				//!!!when export from src, find resource desc and add source texture to CFTexture list!
+				for (int i = 0; i < Textures->GetCount(); ++i)
+				{
+					nString FileName = nString("Export:Textures/") + Textures->Get(i).GetValue<CStrID>().CStr();
+					if (!IsFileAdded(FileName)) FilesToPack.InsertSorted(FileName);
+				}
+			}
+
+			Data::CData* pValue;
+
+			if (AttrDesc->Get(pValue, CStrID("Material")))
+				if (!ProcessMaterialDesc(pValue->GetValue<CStrID>().CStr())) FAIL;
+
+			//!!!when export from src, find resource desc and add source Model to CFModel list!
+			if (AttrDesc->Get(pValue, CStrID("Mesh")))
+			{
+				nString FileName = nString("Export:Meshes/") + pValue->GetValue<CStrID>().CStr() + ".nvx2"; //!!!change extension!
+				if (!IsFileAdded(FileName)) FilesToPack.InsertSorted(FileName);
+			}
+
+			//!!!when export from src, find resource desc and add source BT to CFTerrain list!
+			if (AttrDesc->Get(pValue, CStrID("CDLODFile")))
+			{
+				nString FileName = nString("Export:Terrain/") + pValue->GetValue<CStrID>().CStr() + ".cdlod";
+				if (!IsFileAdded(FileName)) FilesToPack.InsertSorted(FileName);
+			}
+		}
+	}
+
+	Data::PParams Children;
+	if (NodeDesc.Get(Children, CStrID("Children")))
+		for (int i = 0; i < Children->GetCount(); ++i)
+			if (!ProcessSceneNodeRefs(*Children->Get(i).GetValue<Data::PParams>())) FAIL;
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+// Always processed from Src
+bool ProcessSceneResource(const nString& SrcFilePath, const nString& ExportFilePath)
+{
+	if (IsFileAdded(ExportFilePath)) OK;
+
+	Data::PParams Desc = DataSrv->LoadHRD(SrcFilePath, false);
+	if (!Desc.IsValid()) FAIL;
+
+	if (ExportDescs)
+	{
+		IOSrv->CreateDirectory(ExportFilePath.ExtractDirName());
+		IO::CFileStream File;
+		if (!File.Open(ExportFilePath, IO::SAM_WRITE)) FAIL;
+		IO::CBinaryWriter Writer(File);
+		Writer.WriteParams(*Desc, *DataSrv->GetDataScheme(CStrID("SceneNode")));
+		File.Close();
+	}
+
+	FilesToPack.InsertSorted(ExportFilePath);
+
+	return ProcessSceneNodeRefs(*Desc);
+}
+//---------------------------------------------------------------------
+
+bool ProcessDesc(const nString& SrcFilePath, const nString& ExportFilePath)
+{
+	if (IsFileAdded(ExportFilePath)) OK;
+
+	if (ExportDescs)
+	{
+		IOSrv->CreateDirectory(ExportFilePath.ExtractDirName());
+		if (!DataSrv->SavePRM(ExportFilePath, DataSrv->LoadHRD(SrcFilePath, false))) FAIL;
+	}
+	FilesToPack.InsertSorted(ExportFilePath);
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool ProcessEntity(const Data::CParams& EntityDesc)
+{
+	Data::PParams Attrs;
+	if (!EntityDesc.Get<Data::PParams>(Attrs, CStrID("Attrs"))) OK;
+
+	nString AttrValue;
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("UIDesc")))
+		if (!ProcessDesc("Src:Game/UI/" + AttrValue + ".hrd", "Export:Game/UI/" + AttrValue + ".prm"))
+		{
+			n_msg(VR_ERROR, "Error processing UI desc '%s'\n", AttrValue.CStr());
+			FAIL;
+		}
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("AIHintsDesc")))
+		if (!ProcessDesc("Src:Game/AI/Hints/" + AttrValue + ".hrd", "Export:Game/AI/Hints/" + AttrValue + ".prm"))
+		{
+			n_msg(VR_ERROR, "Error processing AI hints desc '%s'\n", AttrValue.CStr());
+			FAIL;
+		}
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("SmartObjDesc")))
+		if (!ProcessDesc("Src:Game/AI/Smarts/" + AttrValue + ".hrd", "Export:Game/AI/Smarts/" + AttrValue + ".prm"))
+		{
+			n_msg(VR_ERROR, "Error processing AI smart object desc '%s'\n", AttrValue.CStr());
+			FAIL;
+		}
+
+	CStrID ItemID = Attrs->Get<CStrID>(CStrID("ItemTplID"), CStrID::Empty);
+	if (ItemID.IsValid())
+		if (!ProcessDesc(nString("Src:Game/Items/") + ItemID.CStr() + ".hrd", nString("Export:Game/Items/") + ItemID.CStr() + ".prm"))
+		{
+			n_msg(VR_ERROR, "Error processing item desc '%s'\n", AttrValue.CStr());
+			FAIL;
+		}
+
+	Data::PDataArray Inventory;
+	if (Attrs->Get<Data::PDataArray>(Inventory, CStrID("Inventory")))
+	{
+		for (int i = 0; i < Inventory->GetCount(); ++i)
+		{
+			ItemID = Inventory->Get<Data::PParams>(i)->Get<CStrID>(CStrID("ID"), CStrID::Empty);
+			if (ItemID.IsValid() &&
+				!ProcessDesc(	nString("Src:Game/Items/") + ItemID.CStr() + ".hrd",
+								nString("Export:Game/Items/") + ItemID.CStr() + ".prm"))
+			{
+				n_msg(VR_ERROR, "Error processing item desc '%s'\n", AttrValue.CStr());
+				FAIL;
+			}
+		}
+	}
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("ActorDesc")))
+		if (!ProcessDescWithParents("Src:Game/AI/Actors/", "Export:Game/AI/Actors/", AttrValue))
+		{
+			n_msg(VR_ERROR, "Error processing AI actor desc '%s'\n", AttrValue.CStr());
+			FAIL;
+		}
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("ScriptClass")))
+	{
+		nString ExportFilePath = "Export:Game/ScriptClasses/" + AttrValue + ".lua";
+		if (!IsFileAdded(ExportFilePath))
+		{
+			if (ExportDescs)
+			{
+				CFLuaIn.Append("Src:Game/ScriptClasses/" + AttrValue + ".lua");
+				CFLuaOut.Append(ExportFilePath);
+			}
+			FilesToPack.InsertSorted(ExportFilePath);
+		}
+	}
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("Script")))
+	{
+		nString ExportFilePath = "Export:Game/Scripts/" + AttrValue + ".lua";
+		if (!IsFileAdded(ExportFilePath))
+		{
+			if (ExportDescs)
+			{
+				CFLuaIn.Append("Src:Game/Scripts/" + AttrValue + ".lua");
+				CFLuaOut.Append(ExportFilePath);
+			}
+			FilesToPack.InsertSorted(ExportFilePath);
+		}
+	}
+
+	CStrID PickShape = Attrs->Get<CStrID>(CStrID("PickShape"), CStrID::Empty);
+	if (PickShape.IsValid())
+		if (!ProcessCollisionShape(	nString("Src:Physics/") + PickShape.CStr() + ".hrd",
+									nString("Export:Physics/") + PickShape.CStr() + ".prm"))
+		{
+			n_msg(VR_ERROR, "Error processing collision shape '%s'\n", PickShape.CStr());
+			FAIL;
+		}
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("Physics")))
+		if (!ProcessPhysicsDesc(nString("Src:Physics/") + AttrValue + ".hrd",
+								nString("Export:Physics/") + AttrValue + ".prm"))
+		{
+			n_msg(VR_ERROR, "Error processing physics desc '%s'\n", AttrValue.CStr());
+			FAIL;
+		}
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("AnimDesc")))
+		if (!ProcessAnimDesc(	nString("Src:Game/Anim/") + AttrValue + ".hrd",
+								nString("Export:Game/Anim/") + AttrValue + ".prm"))
+		{
+			n_msg(VR_ERROR, "Error processing animation desc '%s'\n", AttrValue.CStr());
+			FAIL;
+		}
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("Dialogue")))
+		if (!ProcessDialogue("Src:Game/Dlg/", "Export:Game/Dlg/", AttrValue))
+		{
+			n_msg(VR_ERROR, "Error processing dialogue desc '%s'\n", AttrValue.CStr());
+			FAIL;
+		}
+
+	if (Attrs->Get<nString>(AttrValue, CStrID("SceneFile")))
+		if (!ProcessSceneResource("Src:Scene/" + AttrValue + ".hrd", "Export:Scene/" + AttrValue + ".scn"))
+		{
+			n_msg(VR_ERROR, "Error processing scene resource '%s'\n", AttrValue.CStr());
+			FAIL;
+		}
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool ProcessLevel(const Data::CParams& LevelDesc, const nString& Name)
+{
+	// Add level script
+
+	nString ExportFilePath = "Export:Game/Levels/" + Name + ".lua";
+	if (ExportDescs)
+	{
+		nString SrcFilePath = "Src:Game/Levels/" + Name + ".lua";
+		if (!IsFileAdded(SrcFilePath) && IOSrv->FileExists(SrcFilePath))
+		{
+			CFLuaIn.Append(SrcFilePath);
+			CFLuaOut.Append(ExportFilePath);
+			FilesToPack.InsertSorted(ExportFilePath); //???or after running tool?
+		}
+	}
+	else if (!IsFileAdded(ExportFilePath) && IOSrv->FileExists(ExportFilePath)) FilesToPack.InsertSorted(ExportFilePath);
+
+	// Add navmesh (always exported, no src)
+
+	ExportFilePath = "Export:Game/Levels/" + Name + ".nm";
+	if (!IsFileAdded(ExportFilePath) && IOSrv->FileExists(ExportFilePath)) FilesToPack.InsertSorted(ExportFilePath);
+
+	// Export entities
+
+	Data::PParams SubDesc;
+	if (LevelDesc.Get(SubDesc, CStrID("Entities")))
+	{
+		for (int i = 0; i < SubDesc->GetCount(); ++i)
+		{
+			const Data::CParam& EntityPrm = SubDesc->Get(i);
+			if (!EntityPrm.IsA<Data::PParams>()) continue;
+			Data::PParams EntityDesc = EntityPrm.GetValue<Data::PParams>();
+			n_msg(VR_INFO, "Processing entity '%s'...\n", EntityPrm.GetName().CStr());
+			if (!EntityDesc.IsValid() || !ProcessEntity(*EntityDesc))
+			{
+				n_msg(VR_ERROR, "Error processing entity '%s'\n", EntityPrm.GetName().CStr());
+				FAIL;
+			}
+		}
+	}
+
+	OK;
+}
+//---------------------------------------------------------------------
--- branches/Dev/Tools/Src/BBuilder/Pack_OLD.cpp
+++ branches/Dev/Tools/Src/BBuilder/Pack_OLD.cpp
@@ -1,99 +1,3 @@
-#include <Data/FS/NpkTOC.h>
-#include <Data/Streams/FileStream.h>
-#include <Data/FSBrowser.h>
-
-using namespace Data;
-
-inline const char* StripRootPath(const char* Path, const char* RootPath)
-{
-	n_assert(RootPath);
-	size_t RootPathLen = strlen(RootPath);
-	if (!strncmp(RootPath, Path, RootPathLen) && (strlen(Path) > (RootPathLen + 1)))
-		return &(Path[RootPathLen + 1]);
-	return NULL;
-}
-//---------------------------------------------------------------------
-
-void FilterByFolder(const nString& Folder, nArray<nString>& In, nArray<nString>& Out)
-{
-	nString FolderL = Folder;
-	FolderL.ToLower();
-
-	for (int i = In.Size() - 1; i >= 0; i--)
-	{
-		nString& InStr = In[i];
-		InStr.ToLower();
-
-		if (InStr.IsEmpty())
-		{
-			In.Erase(i);
-			continue;
-		}
-		const char* pStripped = StripRootPath(InStr.Get(), FolderL.Get());
-		if (pStripped)
-		{
-			Out.Append(pStripped);
-			In.Erase(i);
-		}
-	}
-}
-//---------------------------------------------------------------------
-
-bool AddFileToTOC(const nString& FullEntryName, CNpkTOC& TOCObj, int& Offset)
-{
-	Data::CFileStream File;
-	if (File.Open(FullEntryName, Data::SAM_READ))
-	{
-		int FileLength = File.GetSize();
-		File.Close();
-
-		nString EntryName = FullEntryName.ExtractFileName();
-		EntryName.ToLower();
-		n_printf("-> Adding file '%s' at %d len %d\n", EntryName.Get(), Offset, FileLength);
-		TOCObj.AddFileEntry(EntryName.Get(), Offset, FileLength);
-		Offset += FileLength;
-	}
-	else n_printf("*** ERROR: Could not open file '%s', skipping...\n", FullEntryName.Get());
-	return true;
-}
-//---------------------------------------------------------------------
-
-bool AddFilesToTOC(nArray<nString>& Files, CNpkTOC& TOCObj, int& Offset)
-{
-	// Add files that are located directly in current directory
-	nString DirName = TOCObj.GetCurrentDirEntry()->GetFullName() + "/";
-	for (int i = Files.Size() - 1; i >= 0; i--)
-	{
-		//if (Files[i].ExtractFileName() == Files[i])
-		if (Files[i].FindCharIndex('/') == -1)
-		{
-			AddFileToTOC(DirName + Files[i], TOCObj, Offset);
-			Files.Erase(i);
-		}
-	}
-
-	while (Files.Size() > 0)
-	{
-		nString DirName = Files[0].SubString(0, Files[0].FindCharIndex('/'));
-		DirName.ToLower();
-		
-		n_printf("-> Writing NPK directory '%s'\n", DirName.Get());
-		
-		TOCObj.BeginDirEntry(DirName.Get());
-		
-		nArray<nString> DirFiles;
-		FilterByFolder(DirName, Files, DirFiles);
-		AddFilesToTOC(DirFiles, TOCObj, Offset);
-		
-		n_printf("-> End directory %s\n", DirName.Get());
-		
-		TOCObj.EndDirEntry();
-	}
-
-	return true;
-}
-//---------------------------------------------------------------------
-
 bool AddDirectoryToTOC(const nString& DirName, CNpkTOC& TOCObj, int& Offset)
 {
 	bool Result = true;
@@ -148,91 +52,81 @@ bool AddDirectoryToTOC(const nString& DirName, CNpkTOC& TOCObj, int& Offset)
 }
 //---------------------------------------------------------------------
 
-void WriteTOCEntry(Data::CFileStream* pFile, CNpkTOCEntry* tocEntry)
+void WriteTOCEntry(Data::CFileStream* pFile, CNpkTOCEntry* pTOCEntry)
 {
 	n_assert(pFile);
-	n_assert(tocEntry);
+	n_assert(pTOCEntry);
 
-	Data::EFSEntryType entryType = tocEntry->GetType();
-	const char* entryName = tocEntry->GetName();
-	int entryNameLen = strlen(entryName);
+	Data::EFSEntryType entryType = pTOCEntry->GetType();
+	const char* pEntryName = pTOCEntry->GetName();
+	int EntryNameLen = strlen(pEntryName);
 
 	if (Data::FSE_DIR == entryType)
 	{
-		//n_printf("=> Writing dir entry '%s'\n", entryName);
-
-		// write a directory entry, and recurse
-		int blockLen = sizeof(short) + entryNameLen;
+		int blockLen = sizeof(short) + EntryNameLen;
 		pFile->Put<int>('DIR_');
 		pFile->Put<int>(blockLen);
-		pFile->Put<short>(entryNameLen);
-		pFile->Write(entryName, entryNameLen);
+		pFile->Put<short>(EntryNameLen);
+		pFile->Write(pEntryName, EntryNameLen);
 
-		CNpkTOCEntry* curSubEntry = tocEntry->GetFirstEntry();
+		CNpkTOCEntry* curSubEntry = pTOCEntry->GetFirstEntry();
 		while (curSubEntry)
 		{
 			WriteTOCEntry(pFile, curSubEntry);
-			curSubEntry = tocEntry->GetNextEntry(curSubEntry);
+			curSubEntry = pTOCEntry->GetNextEntry(curSubEntry);
 		}
 
-		// write a final directory end marker
 		pFile->Put<int>('DEND');
 		pFile->Put<int>(0);
 	}
 	else if (Data::FSE_FILE == entryType)
 	{
-		//n_printf("=> Writing file entry '%s', offset %d len %d\n", entryName, entryFileOffset, entryFileLength);
-
-		// write a file entry
-		int blockLen = 2 * sizeof(int) + sizeof(short) + entryNameLen;
+		int blockLen = 2 * sizeof(int) + sizeof(short) + EntryNameLen;
 		pFile->Put<int>('FILE');
 		pFile->Put<int>(blockLen);
-		pFile->Put<int>(tocEntry->GetFileOffset());
-		pFile->Put<int>(tocEntry->GetFileLength());
-		pFile->Put<short>(entryNameLen);
-		pFile->Write(entryName, entryNameLen);
+		pFile->Put<int>(pTOCEntry->GetFileOffset());
+		pFile->Put<int>(pTOCEntry->GetFileLength());
+		pFile->Put<short>(EntryNameLen);
+		pFile->Write(pEntryName, EntryNameLen);
 	}
 }
 //---------------------------------------------------------------------
 
-bool WriteEntryData(Data::CFileStream* pFile, CNpkTOCEntry* tocEntry, int dataBlockOffset, int& dataLen)
+bool WriteEntryData(Data::CFileStream* pFile, CNpkTOCEntry* pTOCEntry, int dataBlockOffset, int& dataLen)
 {
     n_assert(pFile);
-    n_assert(tocEntry);
+    n_assert(pTOCEntry);
 
-	Data::EFSEntryType entryType = tocEntry->GetType();
-    const char* entryName = tocEntry->GetName();
+	Data::EFSEntryType entryType = pTOCEntry->GetType();
+    const char* pEntryName = pTOCEntry->GetName();
 
     if (Data::FSE_DIR == entryType)
     {
-        //n_printf("=> enter dir '%s'\n", entryName);
-
-        // a dir entry, just recurse
-        CNpkTOCEntry* curSubEntry = tocEntry->GetFirstEntry();
+        CNpkTOCEntry* curSubEntry = pTOCEntry->GetFirstEntry();
         while (curSubEntry)
         {
             if (!WriteEntryData(pFile, curSubEntry, dataBlockOffset, dataLen))
                 return false;
-            curSubEntry = tocEntry->GetNextEntry(curSubEntry);
+            curSubEntry = pTOCEntry->GetNextEntry(curSubEntry);
         }
     }
     else if (Data::FSE_FILE == entryType)
     {
         // make sure the file is still consistent with the toc data
-        n_assert(pFile->GetPosition() == (dataBlockOffset + tocEntry->GetFileOffset()));
+        n_assert(pFile->GetPosition() == (dataBlockOffset + pTOCEntry->GetFileOffset()));
 
         // get the full source path name
-        nString fileName = tocEntry->GetFullName();
+        nString fileName = pTOCEntry->GetFullName();
 
         // read source file data
 		Data::CFileStream SrcFile;
 		if (SrcFile.Open(fileName, Data::SAM_READ))
         {
             // allocate buffer for file and file contents
-            char* buffer = n_new_array(char, tocEntry->GetFileLength());
-            int bytesRead = SrcFile.Read(buffer, tocEntry->GetFileLength());
+            char* buffer = n_new_array(char, pTOCEntry->GetFileLength());
+            int bytesRead = SrcFile.Read(buffer, pTOCEntry->GetFileLength());
             SrcFile.Close();
-            if (bytesRead != tocEntry->GetFileLength())
+            if (bytesRead != pTOCEntry->GetFileLength())
             {
                 n_delete(buffer);
                 n_printf("Error reading file '%s'!\n", fileName.Get());
@@ -240,14 +134,14 @@ bool WriteEntryData(Data::CFileStream* pFile, CNpkTOCEntry* tocEntry, int dataBl
             }
 
             // write buffer to target pFile
-            int bytesWritten = pFile->Write(buffer, tocEntry->GetFileLength());
+            int bytesWritten = pFile->Write(buffer, pTOCEntry->GetFileLength());
             n_delete(buffer);
-            if (bytesWritten != tocEntry->GetFileLength())
+            if (bytesWritten != pTOCEntry->GetFileLength())
             {
                 n_printf("Error writing to target file!\n");
                 return false;
             }
-            dataLen += tocEntry->GetFileLength();
+            dataLen += pTOCEntry->GetFileLength();
         }
         else
         {
