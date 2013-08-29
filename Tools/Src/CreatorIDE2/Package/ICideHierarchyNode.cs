542bb347faa11d6ae97c04eaef10ff9f36aff329
victor.sushko <victor.sushko@gmail.com>
2013-08-29 06:43:29 +0000

    Changing folders' BuildAction property recursively.

---
+++ Tools/Src/CreatorIDE2/Package/ICideHierarchyNode.cs
@@ -0,0 +1,10 @@
+﻿namespace CreatorIDE.Package
+{
+    internal interface ICideHierarchyNode
+    {
+        CideBuildAction BuildAction { get; }
+        CideBuildAction EffectiveBuildAction { get; }
+
+        void OnBuildActionChanged();
+    }
+}
