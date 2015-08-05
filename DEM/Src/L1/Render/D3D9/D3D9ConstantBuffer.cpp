5d4f53250d7416bc3ad8458474aa8ea2d2b83cda
niello11 <niello11@gmail.com>
2015-08-05 08:05:34 +0000

    Constant buffers progress

--- DEM/Src/L1/Render/D3D9/D3D9ConstantBuffer.cpp
+++ DEM/Src/L1/Render/D3D9/D3D9ConstantBuffer.cpp
@@ -7,27 +7,45 @@ namespace Render
 __ImplementClass(Render::CD3D9ConstantBuffer, 'CB09', Render::CConstantBuffer);
 
 //!!!???assert destroyed?!
-bool CD3D9ConstantBuffer::Create(/*ID3D11Buffer* pCB, ID3D11DeviceContext* pD3DDeviceCtx, bool StoreRAMCopy*/)
+bool CD3D9ConstantBuffer::Create()
 {
 	//if (!pCB || !pD3DDeviceCtx) FAIL;
 
+	NOT_IMPLEMENTED;
 	OK;
 }
 //---------------------------------------------------------------------
 
 void CD3D9ConstantBuffer::InternalDestroy()
 {
+	NOT_IMPLEMENTED;
 }
 //---------------------------------------------------------------------
 
 bool CD3D9ConstantBuffer::BeginChanges()
 {
+	NOT_IMPLEMENTED;
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool CD3D9ConstantBuffer::SetFloat(DWORD Offset, const float* pData, DWORD Count)
+{
+	NOT_IMPLEMENTED;
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool CD3D9ConstantBuffer::SetInt(DWORD Offset, const int* pData, DWORD Count)
+{
+	NOT_IMPLEMENTED;
 	OK;
 }
 //---------------------------------------------------------------------
 
 bool CD3D9ConstantBuffer::CommitChanges()
 {
+	NOT_IMPLEMENTED;
 	OK;
 }
 //---------------------------------------------------------------------
