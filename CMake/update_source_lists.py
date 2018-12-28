7743bb67f73d004fb4e7ce371c558268ee605c8a
Vladimir Orlov <niello@bk.ru>
2018-12-28 17:06:16 +0200

    CMake: lua added

--- CMake/update_source_lists.py
+++ CMake/update_source_lists.py
@@ -2,25 +2,25 @@ import os
 
 PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-def update_lists_cpp(var_name, out_file_name, source_folder):
+def update_lists_cpp(var_name, out_file_path, source_folder, root_folder):
 	header_list = ""
 	source_list = ""
 	for dirpath, dirnames, files in os.walk(source_folder):
 		for file_name in files:
 			ext = os.path.splitext(file_name)[1]
 			if ext == '.cpp' or ext == '.cc' or ext == '.c':
-				source_list += ("\n\t" + os.path.relpath(os.path.join(dirpath, file_name), PROJECT_FOLDER))
+				source_list += ("\n\t" + os.path.relpath(os.path.join(dirpath, file_name), root_folder))
 			elif ext == '.h':
-				header_list += ("\n\t" + os.path.relpath(os.path.join(dirpath, file_name), PROJECT_FOLDER))
+				header_list += ("\n\t" + os.path.relpath(os.path.join(dirpath, file_name), root_folder))
 
 	out_text = 'set({}_HEADERS{}\n)\n\nset({}_SOURCES{}\n)'.format(var_name, header_list.replace("\\", "/"), var_name, source_list.replace("\\", "/"))
 
-	out_file_path = os.path.join(PROJECT_FOLDER, "CMake", out_file_name)
 	with open(out_file_path, "w") as file:
 		file.write(out_text)
 
 
 if __name__ == "__main__":
-	update_lists_cpp("DEM_L1_LOW", "DEMLow.cmake", os.path.join(PROJECT_FOLDER, "DEM", "Low", "src"))
-	update_lists_cpp("DEM_L2_GAME", "DEMGame.cmake", os.path.join(PROJECT_FOLDER, "DEM", "Game", "src"))
-	update_lists_cpp("DEM_L3_RPG", "DEMRPG.cmake", os.path.join(PROJECT_FOLDER, "DEM", "RPG", "src"))
+	update_lists_cpp("DEM_L1_LOW", os.path.join(PROJECT_FOLDER, "CMake", "DEMLow.cmake"), os.path.join(PROJECT_FOLDER, "DEM", "Low", "src"), PROJECT_FOLDER)
+	update_lists_cpp("DEM_L2_GAME", os.path.join(PROJECT_FOLDER, "CMake", "DEMGame.cmake"), os.path.join(PROJECT_FOLDER, "DEM", "Game", "src"), PROJECT_FOLDER)
+	update_lists_cpp("DEM_L3_RPG", os.path.join(PROJECT_FOLDER, "CMake", "DEMRPG.cmake"), os.path.join(PROJECT_FOLDER, "DEM", "RPG", "src"), PROJECT_FOLDER)
+	update_lists_cpp("LUA", os.path.join(PROJECT_FOLDER, "Deps", "lua", "src.cmake"), os.path.join(PROJECT_FOLDER, "Deps", "lua", "src"), os.path.join(PROJECT_FOLDER, "Deps", "lua"))
---
+++ Deps/lua/src.cmake
@@ -0,0 +1,60 @@
+set(LUA_HEADERS
+	src/lapi.h
+	src/lauxlib.h
+	src/lcode.h
+	src/ldebug.h
+	src/ldo.h
+	src/lfunc.h
+	src/lgc.h
+	src/llex.h
+	src/llimits.h
+	src/lmem.h
+	src/lobject.h
+	src/lopcodes.h
+	src/lparser.h
+	src/lstate.h
+	src/lstring.h
+	src/ltable.h
+	src/ltm.h
+	src/lua.h
+	src/luaconf.h
+	src/lualib.h
+	src/lundump.h
+	src/lvm.h
+	src/lzio.h
+)
+
+set(LUA_SOURCES
+	src/lapi.c
+	src/lauxlib.c
+	src/lbaselib.c
+	src/lcode.c
+	src/ldblib.c
+	src/ldebug.c
+	src/ldo.c
+	src/ldump.c
+	src/lfunc.c
+	src/lgc.c
+	src/linit.c
+	src/liolib.c
+	src/llex.c
+	src/lmathlib.c
+	src/lmem.c
+	src/loadlib.c
+	src/lobject.c
+	src/lopcodes.c
+	src/loslib.c
+	src/lparser.c
+	src/lstate.c
+	src/lstring.c
+	src/lstrlib.c
+	src/ltable.c
+	src/ltablib.c
+	src/ltm.c
+	src/lua.c
+	src/luac.c
+	src/lundump.c
+	src/lvm.c
+	src/lzio.c
+	src/print.c
+)
