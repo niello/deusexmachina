bc480a7bfa1f406aa0096b0b10f766d23bdd9242
Владимир Орлов <vladimir.orlov@playrix.com>
2019-07-23 10:41:47 +0200

    Common code for source list updating

--- CMake/update_source_lists.py
+++ CMake/update_source_lists.py
@@ -3,32 +3,13 @@
 # Call it once when source file set changes and commit results.
 #
 import os
+from utils.utils import update_src_lists
 
 PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-def update_lists_cpp(var_name, out_file_path, source_folder, append=False):
-	root_folder = os.path.commonprefix([os.path.dirname(out_file_path), source_folder])
-	header_list = ""
-	source_list = ""
-	for dirpath, dirnames, files in os.walk(source_folder):
-		for file_name in files:
-			ext = os.path.splitext(file_name)[1]
-			if ext == '.cpp' or ext == '.cc' or ext == '.c' or ext == '.cxx':
-				source_list += ("\n\t" + os.path.relpath(os.path.join(dirpath, file_name), root_folder))
-			elif ext == '.h' or ext == '.inc' or ext == '.hpp' or ext == '.inl':
-				header_list += ("\n\t" + os.path.relpath(os.path.join(dirpath, file_name), root_folder))
-
-	out_text = ""
-	if header_list:
-		out_text += 'set({}_HEADERS{}\n)\n\n'.format(var_name, header_list.replace("\\", "/"))
-	if source_list:
-		out_text += 'set({}_SOURCES{}\n)\n\n'.format(var_name, source_list.replace("\\", "/"))
-
-	with open(out_file_path, "a" if append else "w") as file:
-		file.write(out_text)
-
 
 if __name__ == "__main__":
-	update_lists_cpp("DEM_L1_LOW", os.path.join(PROJECT_FOLDER, "CMake", "DEMLow.cmake"), os.path.join(PROJECT_FOLDER, "DEM", "Low", "src"))
-	update_lists_cpp("DEM_L2_GAME", os.path.join(PROJECT_FOLDER, "CMake", "DEMGame.cmake"), os.path.join(PROJECT_FOLDER, "DEM", "Game", "src"))
-	update_lists_cpp("DEM_L3_RPG", os.path.join(PROJECT_FOLDER, "CMake", "DEMRPG.cmake"), os.path.join(PROJECT_FOLDER, "DEM", "RPG", "src"))
+	update_src_lists("DEM_L1_LOW", os.path.join(PROJECT_FOLDER, "CMake", "DEMLow.cmake"), os.path.join(PROJECT_FOLDER, "DEM", "Low", "src"))
+	update_src_lists("DEM_L2_GAME", os.path.join(PROJECT_FOLDER, "CMake", "DEMGame.cmake"), os.path.join(PROJECT_FOLDER, "DEM", "Game", "src"))
+	update_src_lists("DEM_L3_RPG", os.path.join(PROJECT_FOLDER, "CMake", "DEMRPG.cmake"), os.path.join(PROJECT_FOLDER, "DEM", "RPG", "src"))
+	print("Done")
---
+++ CMake/utils/utils.py
@@ -0,0 +1,35 @@
+import os
+
+
+SOURCE_EXT_LIST = ['.cpp', '.cc', '.c', '.cxx']
+HEADER_EXT_LIST = ['.h', '.inc', '.hpp', '.inl']
+
+
+def update_src_lists(var_name, out_file_path, source_folder, append=False,
+                     root_folder=None, source_ext_list=SOURCE_EXT_LIST,
+                     header_ext_list=HEADER_EXT_LIST):
+    if root_folder is None:
+        root_folder = os.path.commonprefix(
+            [os.path.dirname(out_file_path), source_folder])
+    header_list = ""
+    source_list = ""
+    for dirpath, dirnames, files in os.walk(source_folder):
+        for file_name in files:
+            ext = os.path.splitext(file_name)[1]
+            if ext in source_ext_list:
+                source_list += ("\n\t" + os.path.relpath(
+                    os.path.join(dirpath, file_name), root_folder))
+            elif ext in header_ext_list:
+                header_list += ("\n\t" + os.path.relpath(
+                    os.path.join(dirpath, file_name), root_folder))
+
+    out_text = ""
+    if header_list:
+        out_text += 'set({}_HEADERS{}\n)\n\n'.format(
+            var_name, header_list.replace("\\", "/"))
+    if source_list:
+        out_text += 'set({}_SOURCES{}\n)\n\n'.format(
+            var_name, source_list.replace("\\", "/"))
+
+    with open(out_file_path, "a" if append else "w") as file:
+        file.write(out_text)
