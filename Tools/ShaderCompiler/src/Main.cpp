76df73f6404ce3e29b6c8b687548ae10ea9c5c14
Владимир Орлов <vladimir.orlov@playrix.com>
2019-09-22 23:01:32 +0200

    Close DB connection on DLL unloading

--- Tools/ShaderCompiler/src/Main.cpp
+++ Tools/ShaderCompiler/src/Main.cpp
@@ -6,8 +6,9 @@
 
 BOOL WINAPI DllMain(_In_ HINSTANCE hinstDLL, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved)
 {
-	if (fdwReason == DLL_PROCESS_DETACH && !lpvReserved)
+	if (fdwReason == DLL_PROCESS_DETACH)
 	{
+		// DLL is being unloaded for the current process
 		CloseDB();
 	}
 
