92b108431784ee31c3da9e053e149b54c385685d
Владимир Орлов <vladimir.orlov@playrix.com>
2019-09-26 16:57:56 +0200

    CEGUI shader param setup fix

--- DEM/Low/src/UI/CEGUI/DEMRenderer.cpp
+++ DEM/Low/src/UI/CEGUI/DEMRenderer.cpp
@@ -41,9 +41,8 @@ CDEMRenderer::CDEMRenderer(Render::CGPUDriver& GPUDriver, CStrID VertexShaderID,
 	//???pass 3 shaders inside, create render states there etc?
 	ShaderWrapperTextured.reset(new CDEMShaderWrapper(*this, VS, PSRegular, PSOpaque));
 	ShaderWrapperTextured->setupMainTexture("BoundTexture", "LinearSampler");
-	ShaderWrapperTextured->setupParameter("WorldMatrix");
-	ShaderWrapperTextured->setupParameter("ProjectionMatrix");
-	ShaderWrapperTextured->setupParameter("AlphaPercentage"); // TODO: CEGUI implement in shaders
+	ShaderWrapperTextured->setupParameter("WVP");
+	ShaderWrapperTextured->setupParameter("AlphaPercentage");
 
 	//!!!TODO: non-textured shaders!
 	//ShaderWrapperColoured = new CDEMShaderWrapper(*ShaderColoured, this);
