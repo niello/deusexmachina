984cc3fa276a1616b56cd24ead76ab29aa4b2621
Владимир Орлов <vladimir.orlov@playrix.com>
2019-10-13 12:42:00 +0200

    cf-rpath top-level data walking

--- Tools/ContentForge/cf-rpath/Main.cpp
+++ Tools/ContentForge/cf-rpath/Main.cpp
@@ -71,7 +71,80 @@ public:
 			}
 		}
 
-		// ...
+		// Build and verify global parameter table
+
+		std::map<uint32_t, std::string> SerializedGlobals;
+		auto ItEffects = Desc.find(CStrID("Effects"));
+		if (ItEffects != Desc.cend())
+		{
+			if (!ItEffects->second.IsA<Data::CDataArray>())
+			{
+				Log.LogError("'Phases' must be an array of pathes to .eff files");
+				return false;
+			}
+
+			const auto& EffectPathes = ItEffects->second.GetValue<Data::CDataArray>();
+			for (const auto& EffectPathData : EffectPathes)
+			{
+				if (!EffectPathData.IsA<std::string>())
+				{
+					Log.LogError("Wrong data in 'Effects' array, all elements must be strings");
+					return false;
+				}
+
+				auto Path = ResolvePathAliases(EffectPathData.GetValue<std::string>());
+				Log.LogDebug("Opening effect " + Path.generic_string());
+
+				std::ifstream File(Path, std::ios_base::binary);
+				if (!File)
+				{
+					Log.LogError("Can't open effect " + Path.generic_string());
+					return false;
+				}
+
+				// For each shader format merge and verify global metadata
+			}
+		}
+
+		// Process phases
+
+		auto ItPhases = Desc.find(CStrID("Phases"));
+		if (ItPhases == Desc.cend() || !ItPhases->second.IsA<Data::CParams>())
+		{
+			Log.LogError("'Phases' must be a section");
+			return false;
+		}
+
+		const auto& PhaseDescs = ItPhases->second.GetValue<Data::CParams>();
+		for (const auto& PhasePair : PhaseDescs)
+		{
+			if (!PhasePair.second.IsA<Data::CParams>())
+			{
+				Log.LogError("Phase '" + PhasePair.first.ToString() + "' must be a section");
+				return false;
+			}
+
+			const auto& PhaseDesc = PhasePair.second.GetValue<Data::CParams>();
+			//
+		}
+
+		//???warn no render targets/DS-buffers referenced by index?
+		//???use names instead of indices?
+
+		// Write resulting file
+
+		fs::create_directories(DestPath.parent_path());
+
+		std::ofstream File(DestPath, std::ios_base::binary);
+		if (!File)
+		{
+			Log.LogError("Error opening an output file");
+			return false;
+		}
+
+		WriteStream<uint32_t>(File, 'RPTH');                   // Format magic value
+		WriteStream<uint32_t>(File, 0x00010000);               // Version 0.1.0.0
+		WriteStream<uint32_t>(File, SerializedGlobals.size()); // Shader format count
 
 		// Finish task
 
