737fe4c147eeb252aba480d656a1a8bf36d21e97
Владимир Орлов <vladimir.orlov@playrix.com>
2019-10-28 20:58:43 +0200

    Apply for resources and samplers

--- DEM/Low/src/Render/D3D11/USMShaderMetadata.cpp
+++ DEM/Low/src/Render/D3D11/USMShaderMetadata.cpp
@@ -1,6 +1,8 @@
 #include "USMShaderMetadata.h"
 #include <Render/D3D11/D3D11GPUDriver.h>
 #include <Render/D3D11/D3D11ConstantBuffer.h>
+#include <Render/D3D11/D3D11Texture.h>
+#include <Render/D3D11/D3D11Sampler.h>
 
 namespace Render
 {
@@ -35,6 +37,60 @@ bool CUSMConstantBufferParam::IsBufferCompatible(CConstantBuffer& Value) const
 {
 	const auto* pCB = Value.As<CD3D11ConstantBuffer>();
 	return pCB && Type == pCB->GetType() && Size <= pCB->GetSizeInBytes();
+}
+//---------------------------------------------------------------------
+
+bool CUSMResourceParam::Apply(CGPUDriver& GPU, CTexture* pValue) const
+{
+	auto pGPU = GPU.As<CD3D11GPUDriver>();
+	if (!pGPU) FAIL;
+
+	CD3D11Texture* pTex;
+	if (pValue)
+	{
+		pTex = pValue->As<CD3D11Texture>();
+		if (!pTex) FAIL;
+	}
+	else pTex = nullptr;
+
+	// FIXME: use _RegisterCount! Is for arrays?
+
+	for (U8 i = 0; i < ShaderType_COUNT; ++i)
+	{
+		if (_ShaderTypeMask & (1 << i))
+		{
+			if (!pGPU->BindResource(static_cast<EShaderType>(i), _RegisterStart, pTex)) FAIL;
+		}
+	}
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool CUSMSamplerParam::Apply(CGPUDriver& GPU, CSampler* pValue) const
+{
+	auto pGPU = GPU.As<CD3D11GPUDriver>();
+	if (!pGPU) FAIL;
+
+	CD3D11Sampler* pSampler;
+	if (pValue)
+	{
+		pSampler = pValue->As<CD3D11Sampler>();
+		if (!pSampler) FAIL;
+	}
+	else pSampler = nullptr;
+
+	// FIXME: use _RegisterCount! Is for arrays?
+
+	for (U8 i = 0; i < ShaderType_COUNT; ++i)
+	{
+		if (_ShaderTypeMask & (1 << i))
+		{
+			if (!pGPU->BindSampler(static_cast<EShaderType>(i), _RegisterStart, pSampler)) FAIL;
+		}
+	}
+
+	OK;
 }
 //---------------------------------------------------------------------
 
--- DEM/Low/src/Render/D3D9/SM30ShaderMetadata.cpp
+++ DEM/Low/src/Render/D3D9/SM30ShaderMetadata.cpp
@@ -1,6 +1,8 @@
 #include "SM30ShaderMetadata.h"
 #include <Render/D3D9/D3D9GPUDriver.h>
 #include <Render/D3D9/D3D9ConstantBuffer.h>
+#include <Render/D3D9/D3D9Texture.h>
+#include <Render/D3D9/D3D9Sampler.h>
 
 namespace Render
 {
@@ -37,6 +39,60 @@ bool CSM30ConstantBufferParam::IsBufferCompatible(CConstantBuffer& Value) const
 {
 	const auto* pCB = Value.As<CD3D9ConstantBuffer>();
 	return pCB && this == pCB->GetMetadata();
+}
+//---------------------------------------------------------------------
+
+bool CSM30ResourceParam::Apply(CGPUDriver& GPU, CTexture* pValue) const
+{
+	auto pGPU = GPU.As<CD3D9GPUDriver>();
+	if (!pGPU) FAIL;
+
+	CD3D9Texture* pTex;
+	if (pValue)
+	{
+		pTex = pValue->As<CD3D9Texture>();
+		if (!pTex) FAIL;
+	}
+	else pTex = nullptr;
+
+	if (_ShaderTypeMask & ShaderType_Vertex)
+	{
+		if (!pGPU->BindResource(ShaderType_Vertex, _Register, pTex)) FAIL;
+	}
+
+	if (_ShaderTypeMask & ShaderType_Pixel)
+	{
+		if (!pGPU->BindResource(ShaderType_Pixel, _Register, pTex)) FAIL;
+	}
+
+	OK;
+}
+//---------------------------------------------------------------------
+
+bool CSM30SamplerParam::Apply(CGPUDriver& GPU, CSampler* pValue) const
+{
+	auto pGPU = GPU.As<CD3D9GPUDriver>();
+	if (!pGPU) FAIL;
+
+	CD3D9Sampler* pSampler;
+	if (pValue)
+	{
+		pSampler = pValue->As<CD3D9Sampler>();
+		if (!pSampler) FAIL;
+	}
+	else pSampler = nullptr;
+
+	if (_ShaderTypeMask & ShaderType_Vertex)
+	{
+		if (!pGPU->BindSampler(ShaderType_Vertex, _RegisterStart, _RegisterCount, pSampler)) FAIL;
+	}
+
+	if (_ShaderTypeMask & ShaderType_Pixel)
+	{
+		if (!pGPU->BindSampler(ShaderType_Pixel, _RegisterStart, _RegisterCount, pSampler)) FAIL;
+	}
+
+	OK;
 }
 //---------------------------------------------------------------------
 
