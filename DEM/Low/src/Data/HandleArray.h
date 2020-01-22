704f9f79f3017af199547a5816f90ccd2a713773
Vladimir Orlov <niello@bk.ru>
2020-01-22 17:26:29 +0200

    Handle array fixes, optimization, allocation at specified handle

--- DEM/Low/src/Data/HandleArray.h
+++ DEM/Low/src/Data/HandleArray.h
@@ -3,9 +3,9 @@
 
 // Handle array combines a growable array and a pool allocator. For element referencing
 // it provides persistent handles which are never invalidated, unlike pointers or iterators.
-// Handle dereferencing is very fast. Don't store raw pointers obtained through handles.
-// NB: free record linked list reuses records evenly. Dynamic growing keeps even highly
-// dynamic collections from spreading across the memory, if initial size is not very big.
+// Handle dereferencing is very fast, so try to avoid storing raw pointers obtained through handles.
+// Records are reused mostly evenly. Dynamic growing keeps even highly dynamic collections
+// from spreading across the memory, if initial size is not very big.
 // All records are initialized with a prototype or T() by default.
 // Template parameters:
 // T               - value type
@@ -32,7 +32,7 @@ public:
 
 	static constexpr H MAX_CAPACITY = ((1 << IndexBits) - 1);
 
-	typedef struct { H Raw; } CHandle; // A new type is required for the type safety
+	using CHandle = struct { H Raw; }; // A new type is required for the type safety
 
 	static constexpr CHandle INVALID_HANDLE = { MAX_CAPACITY };
 
@@ -48,40 +48,6 @@ protected:
 		H Handle = 0; // Index bits store the next free slot index or INDEX_ALLOCATED value, _not_ this slot index
 	};
 
-	template<typename internal_it>
-	class iterator_tpl
-	{
-	private:
-
-		internal_it _It;
-
-	public:
-
-		using iterator_category = std::bidirectional_iterator_tag;
-
-		using value_type = typename internal_it::value_type;
-		using difference_type = typename internal_it::difference_type;
-		using pointer = typename internal_it::pointer;
-		using reference = typename internal_it::reference;
-
-		iterator_tpl() = default;
-		iterator_tpl(internal_it It) : _It(It) {}
-		iterator_tpl(const iterator_tpl& It) = default;
-		iterator_tpl& operator =(const iterator_tpl& It) = default;
-
-		reference operator *() const { return *_It; }
-		pointer operator ->() const { return _It; }
-		iterator_tpl& operator ++() { do ++_It; while (!IsAllocated()); return *this; }
-		iterator_tpl operator ++(int) { auto Tmp = *this; ++(*this); return Tmp; }
-		iterator_tpl& operator --() { do --_It; while (!IsAllocated()); return *this; }
-		iterator_tpl operator --(int) { auto Tmp = *this; --(*this); return Tmp; }
-
-		bool operator ==(const iterator_tpl& Right) const { return _It == Right._It; }
-		bool operator !=(const iterator_tpl& Right) const { return _It != Right._It; }
-
-		bool IsAllocated() const { return (_It->Handle & INDEX_BITS_MASK) == INDEX_ALLOCATED; }
-	};
-
 	std::vector<CHandleRec> _Records;
 	size_t                  _ElementCount = 0;
 	size_t                  _FirstFreeIndex = MAX_CAPACITY;
@@ -91,11 +57,8 @@ protected:
 	bool IsFull() const { return _LastFreeIndex == MAX_CAPACITY; }
 
 	// Links newly added records to a single linked list of free records
-	void BuildFreeList(size_t StartIndex = 0)
+	void BuildFreeList(size_t StartIndex, size_t EndIndex)
 	{
-		const size_t Size = _Records.size();
-		if (StartIndex >= Size) return;
-
 		if (IsFull())
 		{
 			// No existing free records, build a new free list
@@ -109,10 +72,10 @@ protected:
 		}
 
 		// Reuse bits of the new record are always zero, only set an index part
-		for (auto i = StartIndex; i < Size - 1; ++i)
+		for (auto i = StartIndex; i < EndIndex; ++i)
 			_Records[i].Handle = i + 1;
 
-		_LastFreeIndex = Size - 1;
+		_LastFreeIndex = EndIndex;
 	}
 	//---------------------------------------------------------------------
 
@@ -157,10 +120,77 @@ protected:
 	}
 	//---------------------------------------------------------------------
 
-public:
+	H AllocateEmptyAt(H Handle)
+	{
+		const auto Index = (Handle & INDEX_BITS_MASK);
+		if (Index >= MAX_CAPACITY) return MAX_CAPACITY;
 
-	using iterator = iterator_tpl<typename std::vector<CHandleRec>::iterator>;
-	using const_iterator = iterator_tpl<typename std::vector<CHandleRec>::const_iterator>;
+		const size_t Size = _Records.size();
+		if (Index >= Size)
+		{
+			// Record[Index] is immediately allocated, all other added records are attached to the free list
+			_Records.resize(Index + 1, { _Prototype, 0 } );
+			BuildFreeList(Size, Index - 1);
+		}
+		else
+		{
+			// Fail if target record is already allocated
+			if ((_Records[Index].Handle & INDEX_BITS_MASK) == INDEX_ALLOCATED) return MAX_CAPACITY;
+
+			// Fixup the free list. It is not empty because at least our target record is free.
+			if (Index == _FirstFreeIndex)
+			{
+				// Our record is the first, do standard allocation logic
+				if (_FirstFreeIndex == _LastFreeIndex)
+				{
+					// The last free record was used
+					_FirstFreeIndex = MAX_CAPACITY;
+					_LastFreeIndex = MAX_CAPACITY;
+				}
+				else
+				{
+					// Set the next free index as the first
+					_FirstFreeIndex = (_Records[_FirstFreeIndex].Handle & INDEX_BITS_MASK);
+				}
+			}
+			else
+			{
+				// Our record is not first. Search for it, exclude and fix links.
+				auto PrevIndex = _FirstFreeIndex;
+				do
+				{
+					auto& PrevHandle = _Records[PrevIndex].Handle;
+					auto CurrIndex = (PrevHandle & INDEX_BITS_MASK);
+
+					if (CurrIndex == Index)
+					{
+						if (CurrIndex == _LastFreeIndex)
+						{
+							// Our record was last, fixing list end is enough
+							_LastFreeIndex = PrevIndex;
+						}
+						else
+						{
+							// Link previous record to the next one, skipping current
+							PrevHandle = (PrevHandle & REUSE_BITS_MASK) | (_Records[CurrIndex].Handle & INDEX_BITS_MASK);
+						}
+
+						break;
+					}
+
+					PrevIndex = CurrIndex;
+				}
+				while (PrevIndex != _LastFreeIndex); // Must never happen, 'true' would suffice too
+			}
+		}
+
+		_Records[Index].Handle = (Handle | INDEX_ALLOCATED);
+
+		return Handle;
+	}
+	//---------------------------------------------------------------------
+
+public:
 
 	CHandleArray() = default;
 	CHandleArray(const T& Prototype) : _Prototype(Prototype) {}
@@ -168,14 +198,14 @@ public:
 	CHandleArray(size_t InitialSize)
 		: _Records(std::min<size_t>(InitialSize, MAX_CAPACITY))
 	{
-		BuildFreeList();
+		BuildFreeList(0, _Records.size() - 1);
 	}
 
 	CHandleArray(size_t InitialSize, const T& Prototype)
 		: _Records(std::min<size_t>(InitialSize, MAX_CAPACITY), CHandleRec{ Prototype, 0 })
 		, _Prototype(Prototype)
 	{
-		BuildFreeList();
+		BuildFreeList(0, _Records.size() - 1);
 	}
 
 	CHandle Allocate() { return { AllocateEmpty() }; }
@@ -202,7 +232,7 @@ public:
 		if (Index >= _Records.size()) return;
 
 		auto& Record = _Records[Index];
-		if (((Handle.Raw & REUSE_BITS_MASK) | INDEX_ALLOCATED) != Record.Handle) return;
+		if ((Handle.Raw | INDEX_ALLOCATED) != Record.Handle) return;
 
 		// Clear the record value to default, destroying freed object
 		Record.Value = _Prototype;
@@ -250,6 +280,7 @@ public:
 		_LastFreeIndex = Index;
 	}
 
+	// Explicit reset of exhausted reuse counters. Call when you're sure that no expired handles will become valid.
 	// NB: advanced method, increased risk!
 	void ResetExhaustedReuse()
 	{
@@ -257,62 +288,104 @@ public:
 		NOT_IMPLEMENTED;
 	}
 
+	// Store the value with the specified handle. Useful for serialization and replication.
+	// Fails if target record is already allocated.
 	// NB: advanced method, increased risk!
-	//SetToHandle(Handle, const T& Value)
-
-	// NB: advanced method, increased risk!
-	//SetToHandle(Handle, T&& Value)
-
-	T* GetValue(CHandle Handle)
+	CHandle AllocateWithHandle(H Handle, const T& Value)
 	{
-		const auto Index = (Handle.Raw & INDEX_BITS_MASK);
-		if (Index < _Records.size())
-		{
-			// Check that reuse count is the same and the record is not free
-			auto& Record = _Records[Index];
-			if (((Handle.Raw & REUSE_BITS_MASK) | INDEX_ALLOCATED) == Record.Handle)
-				return &Record.Value;
-		}
+		Handle = AllocateEmptyAt(Handle);
+		if (Handle != MAX_CAPACITY)
+			_Records[Handle & INDEX_BITS_MASK].Value = Value;
+		return { Handle };
+	}
 
-		return nullptr;
+	// Store the value with the specified handle. Useful for serialization and replication.
+	// Fails if target record is already allocated.
+	// NB: advanced method, increased risk!
+	CHandle AllocateWithHandle(H Handle, T&& Value)
+	{
+		Handle = AllocateEmptyAt(Handle);
+		if (Handle != MAX_CAPACITY)
+			_Records[Handle & INDEX_BITS_MASK].Value = std::move(Value);
+		return { Handle };
 	}
 
 	const T* GetValue(CHandle Handle) const
 	{
 		const auto Index = (Handle.Raw & INDEX_BITS_MASK);
-		if (Index < _Records.size())
-		{
-			// Check that reuse count is the same and the record is not free
-			auto& Record = _Records[Index];
-			if (((Handle.Raw & REUSE_BITS_MASK) | INDEX_ALLOCATED) == Record.Handle)
-				return &Record.Value;
-		}
-
-		return nullptr;
+		const bool IsValid = (Index < _Records.size()) && ((Handle.Raw | INDEX_ALLOCATED) == _Records[Index].Handle);
+		return IsValid ? &_Records[Index].Value : nullptr;
 	}
+
+	T*               GetValue(CHandle Handle) { return const_cast<T*>(const_cast<const CHandleArray*>(this)->GetValue(Handle)); }
 
 	constexpr size_t GetMaxCapacity() const { return MAX_CAPACITY; }
 	size_t           GetCurrentCapacity() const { return _Records.size(); }
 
+// *** STL part ***
+
+protected:
+
+	// Iterates through allocated records only
+	template<typename internal_it>
+	class iterator_tpl
+	{
+	private:
+
+		internal_it _It;
+		internal_it _ItEnd; // Otherwise we don't know when to stop advancing through free records
+
+	public:
+
+		using iterator_category = std::bidirectional_iterator_tag;
+
+		using value_type = typename internal_it::value_type;
+		using difference_type = typename internal_it::difference_type;
+		using pointer = typename internal_it::pointer;
+		using reference = typename internal_it::reference;
+
+		iterator_tpl() = default;
+		iterator_tpl(internal_it It, internal_it ItEnd) : _It(It), _ItEnd(ItEnd) {}
+		iterator_tpl(const iterator_tpl& It) = default;
+		iterator_tpl& operator =(const iterator_tpl& It) = default;
+
+		reference operator *() const { return *_It; }
+		pointer operator ->() const { return _It; }
+		iterator_tpl& operator ++() { do ++_It; while (_It != _ItEnd && !IsAllocated()); return *this; }
+		iterator_tpl operator ++(int) { auto Tmp = *this; ++(*this); return Tmp; }
+		iterator_tpl& operator --() { do --_It; while (_It != _ItEnd && !IsAllocated()); return *this; }
+		iterator_tpl operator --(int) { auto Tmp = *this; --(*this); return Tmp; }
+
+		bool operator ==(const iterator_tpl& Right) const { return _It == Right._It; }
+		bool operator !=(const iterator_tpl& Right) const { return _It != Right._It; }
+
+		bool IsAllocated() const { return (_It->Handle & INDEX_BITS_MASK) == INDEX_ALLOCATED; }
+	};
+
+public:
+
+	using iterator = iterator_tpl<typename std::vector<CHandleRec>::iterator>;
+	using const_iterator = iterator_tpl<typename std::vector<CHandleRec>::const_iterator>;
+
 	const_iterator cbegin() const
 	{
-		const_iterator It(_Records.cbegin());
-		const_iterator ItEnd(_Records.cend());
+		const_iterator It(_Records.cbegin(), _Records.cend());
+		const_iterator ItEnd(_Records.cend(), _Records.cend());
 		while (It != ItEnd && !It.IsAllocated()) ++It;
 		return It;
 	}
 
 	iterator begin()
 	{
-		iterator It(_Records.begin());
-		iterator ItEnd(_Records.end());
+		iterator It(_Records.begin(), _Records.end());
+		iterator ItEnd(_Records.end(), _Records.end());
 		while (It != ItEnd && !It.IsAllocated()) ++It;
 		return It;
 	}
 
 	const_iterator   begin() const { return cbegin(); }
-	const_iterator   cend() const { return const_iterator(_Records.cend()); }
-	iterator         end() { return iterator(_Records.end()); }
+	const_iterator   cend() const { return const_iterator(_Records.cend(), _Records.cend()); }
+	iterator         end() { return iterator(_Records.end(), _Records.end()); }
 	const_iterator   end() const { return cend(); }
 	size_t           size() const { return _ElementCount; }
 	bool             empty() const { return !_ElementCount; }
