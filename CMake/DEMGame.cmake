55cc597e091f8f584081d754e581d14444535968
Vladimir Orlov <niello@bk.ru>
2020-03-25 21:10:30 +0200

    Scene component stub

--- CMake/DEMGame.cmake
+++ CMake/DEMGame.cmake
@@ -56,6 +56,7 @@ set(DEM_L2_GAME_HEADERS
 	DEM/Game/src/Game/ECS/EntityTemplate.h
 	DEM/Game/src/Game/ECS/EntityTemplateLoader.h
 	DEM/Game/src/Game/ECS/GameWorld.h
+	DEM/Game/src/Game/ECS/Components/SceneComponent.h
 	DEM/Game/src/Game/Interaction/Ability.h
 	DEM/Game/src/Game/Interaction/Action.h
 	DEM/Game/src/Game/Interaction/ActionContext.h
---
+++ DEM/Game/src/Game/ECS/Components/SceneComponent.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <Data/StringID.h>
+#include <Data/Metadata.h>
+
+// Scene component contains a part of scene hierarchy, including a root node
+// with transform and different possible scene node attributes.
+
+namespace DEM::Game
+{
+
+struct CSceneComponent
+{
+	CStrID AssetID;
+	// root path - empty = add asset to scene root, NOT instead of scene root
+	// initial SRT //???in a node? instantiate asset inside it, not instead of it?
+
+	// root node
+};
+
+}
+
+namespace DEM::Meta
+{
+
+template<> inline constexpr auto RegisterClassName<Game::CSceneComponent>() { return "DEM::Game::CSceneComponent"; }
+template<> inline constexpr auto RegisterMembers<Game::CSceneComponent>()
+{
+	return std::make_tuple
+	(
+		Member(1, "AssetID", &Game::CSceneComponent::AssetID, &Game::CSceneComponent::AssetID)
+	);
+}
+
+}
