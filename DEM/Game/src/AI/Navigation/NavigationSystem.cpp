9224cebcaf5922dbf10cfd9d65336c031a96b5c7
Vladimir Orlov <niello@bk.ru>
2020-05-29 15:28:15 +0200

    Nav: smaller debug path points

--- DEM/Game/src/AI/Navigation/NavigationSystem.cpp
+++ DEM/Game/src/AI/Navigation/NavigationSystem.cpp
@@ -510,7 +510,7 @@ void RenderDebugNavigation(DEM::Game::CGameWorld& World, Debug::CDebugDraw& Debu
 					if (dtStatusFailed(Status)) break;
 
 					DebugDraw.DrawLine(From, To, ColorPathLine);
-					DebugDraw.DrawPoint(To, ColorPathCorner, 10);
+					DebugDraw.DrawPoint(To, ColorPathCorner, 6.f);
 					From = To;
 				}
 				while (dtStatusInProgress(Status));
