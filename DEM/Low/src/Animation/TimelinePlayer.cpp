1ace597c4fb278a4c626ba52655494cd5569d265
Владимир Орлов <vladimir.orlov@playrix.com>
2020-08-10 10:07:40 +0200

    Backward timeline playback

--- DEM/Low/src/Animation/TimelinePlayer.cpp
+++ DEM/Low/src/Animation/TimelinePlayer.cpp
@@ -13,41 +13,36 @@ void CTimelinePlayer::PlayInterval(float PrevTime, float CurrTime)
 	const float Duration = _EndTime - _StartTime;
 
 	// Convert previous time from linear player scale to timeline scale
-	const float PrevLoopIndex = std::trunc(PrevTime / Duration);
-	const float PrevLoopStartTime = PrevLoopIndex * Duration;
+	float LoopStartTime = std::floor(PrevTime / Duration) * Duration;
+	float SegmentStartTimeTL = _StartTime + PrevTime - LoopStartTime;
 
-	float SegmentStartTimeTL;
 	if (CurrTime > PrevTime)
 	{
 		// Play forward
-		SegmentStartTimeTL = _StartTime + PrevTime - PrevLoopStartTime;
-		float NextLoopStartTime = PrevLoopStartTime + Duration;
-		while (NextLoopStartTime < CurrTime)
+		LoopStartTime += Duration;
+		while (LoopStartTime < CurrTime)
 		{
 			_pTrack->PlayInterval(SegmentStartTimeTL, _EndTime, false);
 			SegmentStartTimeTL = _StartTime;
-			NextLoopStartTime += Duration;
+			LoopStartTime += Duration;
 		}
 	}
 	else
 	{
 		// Play backward
-		SegmentStartTimeTL = _StartTime + PrevTime - PrevLoopStartTime; //calc from end?
-		/*float NextLoopStartTime = PrevLoopStartTime + Duration;
-		while (NextLoopStartTime < CurrTime)
+		while (LoopStartTime > CurrTime)
 		{
 			_pTrack->PlayInterval(SegmentStartTimeTL, _StartTime, false);
 			SegmentStartTimeTL = _EndTime;
-			NextLoopStartTime += Duration;
-		}*/
+			LoopStartTime -= Duration;
+		}
 	}
 
 	// Convert current time from linear player scale to timeline scale
-	const float CurrLoopIndex = std::trunc(CurrTime / Duration);
-	const float CurrLoopStartTime = CurrLoopIndex * Duration;
+	const float CurrLoopStartTime = std::floor(CurrTime / Duration) * Duration;
 	const float CurrTimeTL = _StartTime + CurrTime - CurrLoopStartTime;
 
-	// The last segment finishes exactly at the current time
+	// The last segment ends exactly at the current time
 	_pTrack->PlayInterval(SegmentStartTimeTL, CurrTimeTL, true);
 }
 //---------------------------------------------------------------------
