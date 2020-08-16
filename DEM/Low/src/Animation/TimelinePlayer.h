6f56e2b5a6bf224a5f819880d407b299f3a4f275
Vladimir Orlov <niello@bk.ru>
2020-08-16 21:22:34 +0200

    Comments cleaned up

--- DEM/Low/src/Animation/TimelinePlayer.h
+++ DEM/Low/src/Animation/TimelinePlayer.h
@@ -24,8 +24,6 @@ protected:
 	float _CurrTime = 0.f;
 	float _Speed = 1.f;
 
-	//???float _StopTime? When stop playback. How to handle negative (reverse)?
-
 	//???static? or don't pass args?
 	void PlayInterval(float PrevTime, float CurrTime);
 
@@ -36,23 +34,15 @@ public:
 	void SetEndTime(float Time);
 	void SetSpeed(float Speed) { _Speed = Speed; }
 
-	//!!!when set timeline track, must update _StartTime to zero and _EndTime to the last clip end!
-
-	// start time (def 0), end time (def inf), loop count (0 - infinite), speed, negative for reverse
-	//???start, end times - Play() args or player params?
-	//???speed and loop count - Play() args? Not settings of the player itself? Anyway must remember them, but when to set?
-
 	void Update(float dt) { _CurrTime += dt; PlayInterval(_PrevTime, _CurrTime); }
+
+	// Need play, pause, speed, reverse, loop and loop limit
+	//???float _StopTime? When stop playback. How to handle negative (reverse)?
+	// SetTime (move prev and curr), Rewind(dir?) (move curr only to play interval instead of skipping)
+	// Play, Pause, IsPlaying etc
 	//!!!must fix too big times if has looping! can also stop after the last clip (if no current clip was found on any track)!
 
-	// SetTime (move prev and curr), Rewind(dir?) (move curr only to play interval instead of skipping)
-
-	// Play, Pause, IsPlaying etc
-
-	//???how to handle multiple loops inside the same frame?
-
 	/*
-
 	bool  Play() { _Paused = !_Clip; return !_Paused; }
 	void  Stop() { _Paused = true; _CurrTime = 0.f; }
 	void  Pause() { _Paused = true; }
@@ -60,7 +50,6 @@ public:
 	float GetSpeed() const { return _Speed; }
 	bool  IsLooped() const { return _Loop; }
 	bool  IsPlaying() const { return !_Paused; }
-
 	*/
 };
 
