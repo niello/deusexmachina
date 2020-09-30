903ef9b71e2c15ce928ddbbe479598f0dddc588b
Владимир Орлов <vladimir.orlov@playrix.com>
2020-09-30 12:52:12 +0200

    Sol table access fixed

--- DEM/Game/src/Game/GameSession.cpp
+++ DEM/Game/src/Game/GameSession.cpp
@@ -27,7 +27,7 @@ CGameSession::CGameSession()
 	//!!!it doesn't, so session API is not very useful, at least FindFeature, becaus it requires us to
 	//get RTTI and write switch-case for every derived type, which is stupid.
 	auto SessionClassMeta = _ScriptState.new_usertype<CGameSession>("CGameSession"
-		, sol::meta_function::index, [](CGameSession& Self, const char* pKey) { return Self._ScriptFields[pKey]; }
+		, sol::meta_function::index, [](CGameSession& Self, sol::stack_object Key) { return sol::object(Self._ScriptFields[Key]); }
 		, "FindFeature", sol::overload(
 			static_cast<::Core::CRTTIBaseClass* (CGameSession::*)(const char*) const>(&CGameSession::FindFeature)
 			, static_cast<::Core::CRTTIBaseClass* (CGameSession::*)(CStrID) const>(&CGameSession::FindFeature))
