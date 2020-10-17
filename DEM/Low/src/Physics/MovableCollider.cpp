a14cff5e4e1a483e227abb6034113195312958f6
Vladimir Orlov <niello@bk.ru>
2020-10-17 20:11:53 +0200

    Scaled collision shape unsharing WIP

--- DEM/Low/src/Physics/MovableCollider.cpp
+++ DEM/Low/src/Physics/MovableCollider.cpp
@@ -6,25 +6,22 @@
 #include <BulletDynamics/Dynamics/btRigidBody.h>
 #include <BulletCollision/CollisionShapes/btCollisionShape.h>
 
-//!!!DBG TMP!
-#include <Math/TransformSRT.h>
-
 namespace Physics
 {
 
 // Physics object doesn't know the source of its transform, it only stores an incoming copy
-class CKinematicMotionState: public btMotionState
+ATTRIBUTE_ALIGNED16(class)
+CKinematicMotionState: public btMotionState
 {
-protected:
+public:
 
 	btTransform _Tfm;
-
-public:
 
 	BT_DECLARE_ALIGNED_ALLOCATOR();
 
 	CKinematicMotionState(const matrix44& InitialTfm, const vector3& Offset) { SetTransform(InitialTfm, Offset); }
 
+	//???FIXME: need this? need initial tfm? high chances that identity is passed because world tfm is not yet calculated!
 	void SetTransform(const matrix44& NewTfm, const vector3& Offset)
 	{
 		_Tfm = TfmToBtTfm(NewTfm);
@@ -92,12 +89,43 @@ void CMovableCollider::SetTransform(const matrix44& Tfm)
 	_pBtObject->forceActivationState(DISABLE_DEACTIVATION);
 	auto pShape = static_cast<CCollisionShape*>(_pBtObject->getCollisionShape()->getUserPointer());
 
-	//!!!DBG TMP!
-	::Math::CTransformSRT SRT(Tfm);
-	_pBtObject->getCollisionShape()->setLocalScaling(VectorToBtVector(SRT.Scale));
-	const vector3 Offset = { pShape->GetOffset().x * SRT.Scale.x, pShape->GetOffset().y * SRT.Scale.y, pShape->GetOffset().z * SRT.Scale.z };
+	vector3& AxisX = Tfm.AxisX();
+	vector3& AxisY = Tfm.AxisY();
+	vector3& AxisZ = Tfm.AxisZ();
 
-	static_cast<CKinematicMotionState*>(static_cast<btRigidBody*>(_pBtObject)->getMotionState())->SetTransform(Tfm, Offset);
+	const float ScalingX = AxisX.Length();
+	const float ScalingY = AxisY.Length();
+	const float ScalingZ = AxisZ.Length();
+
+	constexpr bool IsShapeShared = true;
+	if (IsShapeShared)
+	{
+		const btVector3& ShapeScaling = _pBtObject->getCollisionShape()->getLocalScaling();
+		constexpr float ShapeUnshareThreshold = 0.0001f;
+		if (!n_fequal(ScalingX, ShapeScaling.x(), ShapeUnshareThreshold) ||
+			!n_fequal(ScalingY, ShapeScaling.y(), ShapeUnshareThreshold) ||
+			!n_fequal(ScalingZ, ShapeScaling.z(), ShapeUnshareThreshold))
+		{
+			// unshare shape - create clone, set local scaling, set object shape, addref/release DEM shape objects
+		}
+	}
+
+	//if shape is shared (no local shape?) and local scaling is different from Tfm scaling for more than a threshold
+	//   clone shape, set new scaling
+	//!!!DBG TMP!
+	_pBtObject->getCollisionShape()->setLocalScaling(btVector3(ScalingX, ScalingY, ScalingZ));
+	const vector3 Offset = { pShape->GetOffset().x * ScalingX, pShape->GetOffset().y * ScalingY, pShape->GetOffset().z * ScalingZ };
+
+	//???TODO PERF: mul reciprocals instead of divisions? is it still actual to do 1 div and 3 mul instead of 3 div?
+	//???TODO PERF: optimize origin?
+	auto& OutTfm = static_cast<CKinematicMotionState*>(static_cast<btRigidBody*>(_pBtObject)->getMotionState())->_Tfm;
+	OutTfm.setBasis(
+		btMatrix3x3(
+			AxisX.x / ScalingX, AxisY.x / ScalingY, AxisZ.x / ScalingZ,
+			AxisX.y / ScalingX, AxisY.y / ScalingY, AxisZ.y / ScalingZ,
+			AxisX.z / ScalingX, AxisY.z / ScalingY, AxisZ.z / ScalingZ));
+	OutTfm.setOrigin(VectorToBtVector(Tfm.Translation()));
+	OutTfm.getOrigin() = OutTfm * VectorToBtVector(Offset);
 }
 //---------------------------------------------------------------------
 
