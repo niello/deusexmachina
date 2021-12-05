9a0fed7c5cabd95f3d0a7db61740d24691df08f9
Vladimir Orlov <niello@bk.ru>
2021-12-05 13:35:34 +0200

    Utility for removing trailing zeros from the float string

--- DEM/Low/src/Data/StringUtils.h
+++ DEM/Low/src/Data/StringUtils.h
@@ -1,37 +1,45 @@
 #pragma once
-#ifndef DEM_L1_STRING_UTILS_H
-#define DEM_L1_STRING_UTILS_H
-
 #include <Data/String.h>
 #include <Math/Matrix44.h>
-//#include <Data/Array.h>
 
 // String manipulation and utility functions
 
 namespace StringUtils
 {
-	inline CString	FromInt(I32 Value) { CString Str; Str.Format("%ld", Value); return Str; }
-	inline CString	FromUInt(U32 Value, bool Hex = false) { CString Str; Str.Format(Hex ? "%lx" : "%lu", Value); return Str; }
-	inline CString	FromFloat(float Value) { CString Str; Str.Format("%.6f", Value); return Str; }
-	inline CString	FromBool(bool Value) { return CString(Value ? "true" : "false"); }
-	inline CString	FromVector3(const vector3& v) { CString Str; Str.Format("%.6f,%.6f,%.6f", v.x, v.y, v.z); return Str; }
-	inline CString	FromVector4(const vector4& v) { CString Str; Str.Format("%.6f,%.6f,%.6f,%.6f", v.x, v.y, v.z, v.w); return Str; }
-	CString			FromMatrix44(const matrix44& m);
 
-	inline int		ToInt(const char* pStr) { return atoi(pStr); }
-	inline float	ToFloat(const char* pStr) { return (float)atof(pStr); }
-	bool			ToBool(const char* pStr);
+inline CString	FromInt(I32 Value) { CString Str; Str.Format("%ld", Value); return Str; }
+inline CString	FromUInt(U32 Value, bool Hex = false) { CString Str; Str.Format(Hex ? "%lx" : "%lu", Value); return Str; }
+inline CString	FromFloat(float Value) { CString Str; Str.Format("%.6f", Value); return Str; }
+inline CString	FromBool(bool Value) { return CString(Value ? "true" : "false"); }
+inline CString	FromVector3(const vector3& v) { CString Str; Str.Format("%.6f,%.6f,%.6f", v.x, v.y, v.z); return Str; }
+inline CString	FromVector4(const vector4& v) { CString Str; Str.Format("%.6f,%.6f,%.6f,%.6f", v.x, v.y, v.z, v.w); return Str; }
+CString			FromMatrix44(const matrix44& m);
 
-	char*			LastOccurrenceOf(const char* pStrStart, const char* pStrEnd, const char* pSubStr, UPTR SubStrLen = 0);
-	bool			MatchesPattern(const char* pStr, const char* pPattern);
-	//CString			Trim(const char* CharSet = DEM_WHITESPACE, bool Left = true, bool Right = true) const;
+inline int		ToInt(const char* pStr) { return atoi(pStr); }
+inline float	ToFloat(const char* pStr) { return (float)atof(pStr); }
+bool			ToBool(const char* pStr);
 
-	UPTR			StripComments(char* pStr, const char* pSingleLineComment = "//", const char* pMultiLineCommentStart = "/*", const char* pMultiLineCommentEnd = "*/"); 
+char*			LastOccurrenceOf(const char* pStrStart, const char* pStrEnd, const char* pSubStr, UPTR SubStrLen = 0);
+bool			MatchesPattern(const char* pStr, const char* pPattern);
+//CString			Trim(const char* CharSet = DEM_WHITESPACE, bool Left = true, bool Right = true) const;
 
-	inline bool		AreEqualCaseInsensitive(const char* pStr1, const char* pStr2)
-	{
-		return pStr1 == pStr2 || (pStr1 && pStr2 && !n_stricmp(pStr1, pStr2));
-	}
+UPTR			StripComments(char* pStr, const char* pSingleLineComment = "//", const char* pMultiLineCommentStart = "/*", const char* pMultiLineCommentEnd = "*/"); 
+
+inline bool AreEqualCaseInsensitive(const char* pStr1, const char* pStr2)
+{
+	return pStr1 == pStr2 || (pStr1 && pStr2 && !n_stricmp(pStr1, pStr2));
 }
+//---------------------------------------------------------------------
 
-#endif
+inline std::string& TrimTrailingDecimalZeros(std::string& Str)
+{
+	const auto DotPos = Str.find_last_of('.');
+	if (DotPos != std::string::npos)
+		while (Str.back() == '0' && Str.size() > (DotPos + 2))
+			Str.pop_back();
+
+	return Str;
+}
+//---------------------------------------------------------------------
+
+}
