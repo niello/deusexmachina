2a6d95cd11da91cc24c914f8fa05629dda87d8aa
Vladimir Orlov <niello@bk.ru>
2023-01-05 00:04:38 +0100

    Scripting: pass CStrID by value

--- DEM/Game/src/Scripting/Sol.h
+++ DEM/Game/src/Scripting/Sol.h
@@ -6,8 +6,13 @@
 
 namespace sol
 {
+// FIXME: sol 3.3.0 - we may want explicit T& to be passed by ref, but sol checks trait for T& when passing T
 template <> struct is_value_semantic_for_function<DEM::Game::HEntity> : std::true_type {};
 template <> struct is_value_semantic_for_function<const DEM::Game::HEntity> : std::true_type {};
 template <> struct is_value_semantic_for_function<DEM::Game::HEntity&> : std::true_type {};
 template <> struct is_value_semantic_for_function<const DEM::Game::HEntity&> : std::true_type {};
+template <> struct is_value_semantic_for_function<CStrID> : std::true_type {};
+template <> struct is_value_semantic_for_function<const CStrID> : std::true_type {};
+template <> struct is_value_semantic_for_function<CStrID&> : std::true_type {};
+template <> struct is_value_semantic_for_function<const CStrID&> : std::true_type {};
 }
