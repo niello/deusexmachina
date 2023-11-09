f25a79c475ebb85b109208ced3075033efada9fb
Vladimir Orlov <niello@bk.ru>
2023-11-09 11:51:29 +0100

    Tools: FBX SDK autoselection

--- CMake/tools-vs2022x86.bat
+++ CMake/tools-vs2022x86.bat
@@ -1,3 +1,3 @@
 python update_source_lists.py
-cmake -G "Visual Studio 17 2022" -A Win32 -S "../Tools" -B "../Tools/build" -DFBX_SDK_PATH="C:\Program Files\Autodesk\FBX\FBX SDK\2020.3.1"
+cmake -G "Visual Studio 17 2022" -A Win32 -S "../Tools" -B "../Tools/build"
 pause
