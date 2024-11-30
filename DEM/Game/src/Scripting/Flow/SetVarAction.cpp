2309ea6d58cde655ccef609fbfa5b13dadd701af
Vladimir Orlov <niello@bk.ru>
2024-11-30 17:54:46 +0100

    SetVarAction storage param

--- DEM/Game/src/Scripting/Flow/SetVarAction.cpp
+++ DEM/Game/src/Scripting/Flow/SetVarAction.cpp
@@ -1,5 +1,7 @@
 #include "SetVarAction.h"
 #include <Scripting/Flow/FlowAsset.h>
+#include <Game/GameSession.h>
+#include <Game/SessionVars.h>
 #include <Core/Factory.h>
 
 namespace DEM::Flow
@@ -8,12 +10,37 @@ FACTORY_CLASS_IMPL(DEM::Flow::CSetVarAction, 'SVRA', Flow::IFlowAction);
 
 static const CStrID sidName("Name");
 static const CStrID sidValue("Value");
+static const CStrID sidStorage("Storage");
+static const CStrID sidLocal("Local");
+static const CStrID sidSession("Session");
+static const CStrID sidPersistent("Persistent");
 
 void CSetVarAction::Update(Flow::CUpdateContext& Ctx)
 {
 	if (const CStrID Name = _pPrototype->Params->Get<CStrID>(sidName, CStrID::Empty))
+	{
 		if (auto* pValueParam = _pPrototype->Params->Find(sidValue))
-			_pPlayer->GetVars().TrySet(Name, pValueParam->GetRawValue());
+		{
+			const CStrID Storage = _pPrototype->Params->Get<CStrID>(sidStorage, sidLocal);
+			if (Storage == sidLocal)
+			{
+				_pPlayer->GetVars().TrySet(Name, pValueParam->GetRawValue());
+			}
+			else if (auto* pSessionVars = Ctx.pSession->FindFeature<Game::CSessionVars>())
+			{
+				if (Storage == sidSession)
+					pSessionVars->Runtime.TrySet(Name, pValueParam->GetRawValue());
+				else if (Storage == sidPersistent)
+					pSessionVars->Persistent.TrySet(Name, pValueParam->GetRawValue());
+				else
+					return Throw(Ctx, "CSetVarAction: unknown storage type " + Storage.ToString(), false);
+			}
+			else
+			{
+				return Throw(Ctx, "CSetVarAction: CSessionVars is missing, can't set global (Session or Persistent) vars", false);
+			}
+		}
+	}
 
 	Goto(Ctx, GetFirstValidLink(*Ctx.pSession, _pPlayer->GetVars()));
 }
