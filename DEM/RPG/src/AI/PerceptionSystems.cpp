27303e0b85eaa174cd79692fc9ba9480fc1d9702
Vladimir Orlov <niello@bk.ru>
2025-03-23 14:12:33 +0100

    Moved vision system to RPG module

--- DEM/RPG/src/AI/PerceptionSystems.cpp
+++ DEM/RPG/src/AI/PerceptionSystems.cpp
@@ -1,6 +1,14 @@
 #include <Game/GameSession.h>
-//#include <Game/ECS/GameWorld.h>
+#include <Game/ECS/GameWorld.h>
+#include <Game/GameLevel.h>
 #include <AI/Perception.h>
+#include <AI/AIStateComponent.h>
+#include <AI/VisionSensorComponent.h>
+
+namespace DEM::Game
+{
+	bool GetTargetFromPhysicsObject(const Physics::CPhysicsObject& Object, CTargetInfo& OutTarget);
+}
 
 namespace DEM::RPG
 {
@@ -18,6 +26,82 @@ AI::EAwareness SenseVisualStimulus(Game::CGameSession& Session, Game::HEntity Se
 	// can apply sensor cheats and buffs here
 
 	return AI::EAwareness::Full;
+}
+//---------------------------------------------------------------------
+
+void ProcessVisionSensors(Game::CGameSession& Session, Game::CGameWorld& World, Game::CGameLevel& Level)
+{
+	World.ForEachEntityInLevelWith<const DEM::AI::CVisionSensorComponent, DEM::AI::CAIStateComponent>(Level.GetID(),
+		[&Session, &Level](auto SensorID, auto& Entity, const DEM::AI::CVisionSensorComponent& Sensor, DEM::AI::CAIStateComponent& AIState)
+	{
+		if (!Sensor.Node || Sensor.MaxRadius <= 0.f) return; // continue
+
+		ZoneScopedN("VisionSensor");
+
+		// skip by sparse update, sensors must be distributed more or less evenly among frames
+
+		//???separate collision world and simplified shapes for sensing? or add sensing collision flags to physics bodies and existing colliders?
+
+		// TODO PERF: could reuse shape and collision flags and perform contactTest manually, but is it really benefical?
+		Level.EnumEntitiesInSphere(Sensor.Node->GetWorldPosition(), Sensor.MaxRadius, /* "Visible"sv */ "Dynamic"sv,
+			[&Session, &Level, &Sensor, SensorID, &AIState](DEM::Game::HEntity StimulusID, const rtm::vector4f& ContactPos)
+		{
+			if (StimulusID == SensorID) return true; // continue
+
+			const auto& SensorTfm = Sensor.Node->GetWorldMatrix();
+			const auto SensorPos = SensorTfm.w_axis;
+			const auto SensorToContact = rtm::vector_sub(ContactPos, SensorPos);
+
+			// Test against max vision distance
+			// FIXME: why physics system reports such contacts?
+			const float DistanceToContactSq = rtm::vector_length_squared3(SensorToContact);
+			if (DistanceToContactSq > Sensor.MaxRadiusSq) return true; // continue
+
+			// Test against max vision angle
+			const auto LookatDir = rtm::vector_normalize3(rtm::vector_neg(SensorTfm.z_axis));
+			const auto ContactDir = rtm::vector_mul(SensorToContact, rtm::scalar_sqrt_reciprocal(DistanceToContactSq));
+			const float CosLookAtStimulus = rtm::vector_dot3(LookatDir, ContactDir);
+			if (CosLookAtStimulus < Sensor.CosHalfMaxFOV) return true; // continue
+
+			// Modify stimulus intensity with peripheral vision and distance coefficients
+			float Modifier = 1.f;
+			if (CosLookAtStimulus < Sensor.CosHalfPerfectFOV)
+				Modifier *= 1.f - (Sensor.CosHalfPerfectFOV - CosLookAtStimulus) / (Sensor.CosHalfPerfectFOV - Sensor.CosHalfMaxFOV);
+			if (DistanceToContactSq > Sensor.PerfectRadiusSq)
+				Modifier *= 1.f - (rtm::scalar_sqrt(DistanceToContactSq) - Sensor.PerfectRadius) / (Sensor.MaxRadius - Sensor.PerfectRadius);
+			n_assert_dbg(Modifier > 0.f);
+
+			// Apply game logic
+			const auto Awareness = DEM::RPG::SenseVisualStimulus(Session, SensorID, StimulusID, Modifier);
+			if (Awareness == DEM::AI::EAwareness::None) return true; // continue
+
+			// Perform line of sight test
+			// TODO PERF:
+			//   can use navmesh raycast as a cheap first chance check, must work good with walls etc
+			//   can offset raycast to target side if it is moving, or try multiple raycasts to sides and upper/lower parts
+			//   can do raycast async, but wait for results if this stimulus is considered by decision making? Or when merging starts?
+			//   raycast can be performed less frequently then a sensor test, especially if the object has passed a raycast test before
+			//   may do async raycasts for objects with previous successfull raycast, i.e. currently visible ones
+			//   must not filter out characters standing one after another! Check only static geometry and dynamic things like doors?
+			if (auto* pPhysicsObject = Level.GetFirstPickIntersection(SensorPos, ContactPos, nullptr, /*"Visible|Static|Dynamic"sv*/ ""sv, SensorID))
+			{
+				DEM::Game::CTargetInfo LOSCollision;
+				DEM::Game::GetTargetFromPhysicsObject(*pPhysicsObject, LOSCollision);
+				if (LOSCollision.Entity != StimulusID) return true; // continue
+			}
+
+			// Register a new stimulus in the AI brain
+			auto& Stimulus = AIState.NewStimuli.emplace_back();
+			Stimulus.Position = ContactPos;
+			Stimulus.SourceID = StimulusID;
+			Stimulus.Awareness = Awareness;
+			//Stimulus.AddedTimestamp - here or later when merging?
+			//Stimulus.UpdatedTimestamp - here or later when merging?
+			//Stimulus.TypeFlags = ...; //!!!must be set in SenseVisualStimulus!
+
+			return true; // continue
+		});
+	});
 }
 //---------------------------------------------------------------------
 
